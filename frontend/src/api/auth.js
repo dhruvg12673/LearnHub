866d9b0e698cf4f246d8c2dcd1f6faa1782c959a
dhruvg12673 <dhruvgothi9324@gmail.com>
2026-01-08 12:19:47 +0530

    Create auth.js

---
+++ frontend/src/api/auth.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8000/api';
+
+export const login = async (username, password) => {
+  const response = await axios.post(`${API_URL}/auth/login`, { username, password });
+  return response.data;
+};
+
+export const signup = async (username, password) => {
+  const response = await axios.post(`${API_URL}/auth/signup`, { username, password });
+  return response.data;
+};
